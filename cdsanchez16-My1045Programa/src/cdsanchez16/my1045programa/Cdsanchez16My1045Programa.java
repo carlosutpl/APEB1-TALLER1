9c32add891ad1c4cca2ee5a926aa44e8f0f0b46c
carlosutpl <carlosdavidsanchezcarrion@gmail.com>
2023-10-19 17:11:56 -0500

    Este es la primera cosa de git

---
+++ cdsanchez16-My1045Programa/src/cdsanchez16/my1045programa/Cdsanchez16My1045Programa.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cdsanchez16.my1045programa;
+
+/**
+ *
+ * @author bigotes20
+ */
+public class Cdsanchez16My1045Programa {
+
+    public static void main(String[] args) {
+        System.out.println("Que pasa calabaza");
+    }
+    
+}
