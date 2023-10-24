2a50fce94123ad0c432bf8bf1b82ed3e43b5b9b9
carlosutpl <carlosdavidsanchezcarrion@gmail.com>
2023-10-23 21:44:22 -0500

    primer commit

---
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto01;
+
+/**
+ *
+ * @author V I C T U S
+ */
+public class Proyecto01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("Ejemplo básico de Java versionado a través de Git Manejado desde GitHub");
+    }
+    
+}
